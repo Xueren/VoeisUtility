d7f0f96b7eb979d8602e5e403d94f07fba909999
Andrew Fannin <andrew.fannin@murraystate.edu>
2012-02-02 16:04:03 -0600

    Added some files that were not versioned.

---
+++ VoeisUploadUtility/src/Main/VoeisUploadUtility.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import Views.Container;
+
+/**
+ *
+ * @author afannin1
+ */
+public class VoeisUploadUtility {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Container container = new Container();  //Opens the JFrame which contains the MainView JPanel
+    }
+}
